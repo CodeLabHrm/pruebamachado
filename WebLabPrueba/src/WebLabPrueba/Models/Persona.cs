a231b84c141ee136d479c114a132d7d21a85e0d4
HUMBERTO <hurima9167@gmail.com>
2016-08-10 21:42:39 -0500

    Cambio 0001

---
+++ WebLabPrueba/src/WebLabPrueba/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLabPrueba.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Type { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
